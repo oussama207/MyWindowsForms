6bb57c39908aeb91c39ddd13bdebf6b5554bc084
oussama-gif <66790652+oussama-gif@users.noreply.github.com>
2021-02-19 14:57:23 +0100

    9

--- MyWindowsForms/FormMain.Designer.cs
+++ MyWindowsForms/FormMain.Designer.cs
@@ -29,21 +29,59 @@ namespace MyWindowsForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
             // 
             // textBox2
             // 
             resources.ApplyResources(this.textBox2, "textBox2");
             this.textBox2.Name = "textBox2";
+            this.toolTip1.SetToolTip(this.textBox2, resources.GetString("textBox2.ToolTip"));
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
             // 
             // button2
             // 
@@ -52,12 +90,42 @@ namespace MyWindowsForms
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // FormMain
             // 
             resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "FormMain";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
@@ -71,8 +139,17 @@ namespace MyWindowsForms
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- MyWindowsForms/FormMain.cs
+++ MyWindowsForms/FormMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MyWindowsForms
 {
@@ -19,20 +20,148 @@ namespace MyWindowsForms
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            this.Icon = Icon.ExtractAssociatedIcon(AppDomain.CurrentDomain.FriendlyName);
+        }
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int a = int.Parse(textBox2.Text);
-            label2.Location = new Point(a,50); 
+            try { 
+                if(textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" )
+                {
+                    MessageBox.Show("Enter all");
+                    return;
+                }
+
+                StreamReader sr = new StreamReader("Data.txt");
+                string strcheck = sr.ReadToEnd();
+                sr.Close();
+                if(strcheck.Contains("ID : " + textBox2.Text + "\n"))
+                {
+                    MessageBox.Show("This ID Exist");
+                    textBox2.Focus();
+                    textBox2.SelectAll();
+                }
+                else
+                {
+                    StreamWriter sw = new StreamWriter("Data.txt", true);
+                    string strPerson = "ID : " +  textBox2.Text + "\n"
+                                      + "Name : " + textBox3.Text + "\n"
+                                       + "Address : " + textBox4.Text + "\n";
+                    sw.WriteLine(strPerson);
+                    sw.Close();
+                    MessageBox.Show("Person Is Added");
+                    foreach(Control c in this.Controls)
+                    {
+                        if (c is TextBox)
+                            c.Text = "";
+                    }
+                    textBox2.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text.Trim() == "")
+                {
+                    MessageBox.Show("Enter id");
+                    return;
+                }
+                else
+                {
+                    StreamReader sr = new StreamReader("Data.txt");
+                    string line = "";
+                    bool found = false;
+                    do
+                    {
+                        line = sr.ReadLine() + '\n';
+                        line += sr.ReadLine() + '\n';
+                        line += sr.ReadLine();
+                        if (line != "\n\n")
+                        {
+                            string[] arrData = line.Split('\n');
+                            arrData[0] = arrData[0].Substring(5);
+                            arrData[1] = arrData[1].Substring(7);
+                            arrData[2] = arrData[2].Substring(10);
+                            if (textBox2.Text == arrData[0])
+                            {
+                                found = true;
+                                textBox3.Text = arrData[1];
+                                textBox4.Text = arrData[2];
+                                break;
+                            }
+                        }
+                        line += sr.ReadLine();
+                    } while (line != null && line != "\n\n");
+                    sr.Close();
+                    if (found == false)
+                    {
+                        MessageBox.Show("not Found");
+                        textBox2.Focus();
+                        textBox2.SelectAll();
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.Font = this.Font;
+            frm.Icon = this.Icon;
+            frm.Size = this.Size;
+            frm.Text = "All Data";
+            TextBox txt = new TextBox();
+            txt.Multiline = true;
+            txt.Dock = DockStyle.Fill;
+            frm.Controls.Add(txt);
+
+            try
+            {
+                StreamReader sr = new StreamReader("Data.txt");
+                string data = "";
+                string tmp = sr.ReadLine();
+                while (tmp != null)
+                {
+                    data += tmp + "\r\n";
+                    tmp = sr.ReadLine();
+
+                }
+                sr.Close();
+                txt.Text = data;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            frm.ShowDialog();
+        }
+
+
+
+
 
         // Show Message
         //MessageBox.Show("Hello");
-
-        // yes yes
-        // omg
 
         // Get/Set text
         // XXX.Text;
@@ -45,6 +174,47 @@ namespace MyWindowsForms
         // XXX.Left = YYY;
         // XXX.Location = new Point(x,y);
 
+        // Events 1
+        // _Load
+        // _Resize
+        // _Move 
+        // _MouseHover
+        // _MouseLeave
 
+        // Events 2
+        // xxx.Focus()
+
+        // Width or Height
+        // Screen.PrimaryScreen.Bounds.Width / Height
+        // this.Width / Height
+
+        // Sive
+        // this.size = new Size(w,h);
+
+        // switch between forms
+        // formName xxx = new formName();
+        //  Application.OpenForms[0].Show();
+        // this.show() / ShowDialog() / Hide() / Close()
+
+        // Create form using code
+        // Form name = new Form()
+        // xxx.StartPosition 
+        // FormStartPosition.CenterScreen
+
+        // Create new component
+        // Component xxx = new Component();
+        // Foemname.Controls.Add(xxx);
+
+        // Check Box
+        // xxx.Checked / ToString()
+        // xxx.ChekedItems
+        // xxx.Items.Add();
+        // xxx.Items.AddRange();
+
+        // Icon
+        // this.Icon = Icon.ExtractAssociatedIcon(AppDomain.CurrentDomain.FriendlyName);
+
+        // Exit 
+        //  Application.Exit();
     }
 }
